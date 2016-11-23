08fb466f97787a0c934bd501185993d5bbde2c3b
Icemanrob <icemanrob@gmail.com>
2016-11-22 17:48:58 -0700

    Updated rules.pl to work with lexicon.

--- rules_CS.pl
+++ rules_CS.pl
@@ -1,10 +1,10 @@
+%%consult(lexicon_original).
 utterance(X) :- sentence(X, []).
 sentence(Start, End) :- nounphrase(Start, Rest, Number), verbphrase(Rest, End, Number).
 nounphrase([Noun | End], End, Number) :- noun(Noun, Number).
 nounphrase([Article, Noun | End], End, Number) :- noun(Noun, Number), article(Article, Number).
 verbphrase([Verb | End], End, Number) :- verb(Verb, Number).
 verbphrase([Verb | Rest], End, Number) :- verb(Verb, Number), nounphrase(Rest, End, _).
-consult("lexicon_original").
 
 %% article(a, singular).
 %% article(these, plural).

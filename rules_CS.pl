604c72a7c0ee2beb9ac5fe91ac5be30179e874e6
Icemanrob <icemanrob@gmail.com>
2016-11-22 17:23:31 -0700

    Context sensitive rules, basic.

---
+++ rules_CS.pl
@@ -0,0 +1,20 @@
+utterance(X) :- sentence(X, []).
+sentence(Start, End) :- nounphrase(Start, Rest, Number), verbphrase(Rest, End, Number).
+nounphrase([Noun | End], End, Number) :- noun(Noun, Number).
+nounphrase([Article, Noun | End], End, Number) :- noun(Noun, Number), article(Article, Number).
+verbphrase([Verb | End], End, Number) :- verb(Verb, Number).
+verbphrase([Verb | Rest], End, Number) :- verb(Verb, Number), nounphrase(Rest, End, _).
+consult("lexicon_original").
+
+%% article(a, singular).
+%% article(these, plural).
+%% article(the, singular).
+%% article(the, plural).
+%% noun(man, singular).
+%% noun(men, plural).
+%% noun(dog, singular).
+%% noun(dogs, plural).
+%% verb(likes, singular).
+%% verb(like, plural).
+%% verb(bites, singular).
+%% verb(bite, plural).

1570746de594bffcd898569718ab33ac9ff50eff
Icemanrob <Icemanrob@gmail.com>
2016-11-27 17:12:53 -0700

    adjectivephrase fuctions recursively

--- rules_CS.pl
+++ rules_CS.pl
@@ -1,15 +1,19 @@
 utterance(X) :- sentence(X, []).
 sentence(Start, End) :- nounphrase(Start, Rest, Number), verbphrase(Rest, End, Number).
+
 noun(Noun, Number) :- noun(_, Noun, Number).
 verb(Verb, Number) :- verb(_, Verb, Number).
 adjective(Adjective) :- adjective(_, Adjective).
 article(Article, Number) :- article(_, Article, Number).
 adverb(Adverb) :- adverb(_, Adverb).
 
-nounphrase([Noun | End], End, Number) :- noun(Noun, Number).
-nounphrase([Adjective, Noun | End], End, Number) :- noun(Noun, Number), adjective(Adjective).
-nounphrase([Article, Noun | End], End, Number) :- noun(Noun, Number), article(Article, Number).
-nounphrase([Article, Adjective, Noun | End], End, Number) :- noun(Noun, Number), article(Article, Number), adjective(Adjective).
+nounphrase(Adjectivephrase, End, Number) :- adjectivephrase(Adjectivephrase, End, Number).
+%%nounphrase([Adjective, Rest | End], End, Number) :- nounphrase(Rest, End, Number), adjectivephrase(Adjective).
+nounphrase([Article | Rest], End, Number) :- article(Article, Number), adjectivephrase(Rest, End, Number).
+
+adjectivephrase([Adjective | Rest], End, Number) :- adjective(Adjective), adjectivephrase(Rest, End, Number).
+adjectivephrase([Noun | End], End, Number) :- noun(Noun, Number).
+
 
 verbphrase([Verb | End], End, Number) :- verb(Verb, Number).
 verbphrase([Verb, Adverb | End], End, Number) :- verb(Verb, Number), adverb(Adverb).

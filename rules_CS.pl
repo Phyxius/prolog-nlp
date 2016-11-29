0849dce9b01a80b3b747ffd07d0adedd0b6628f5
Shea Polansky <env.laser@gmail.com>
2016-11-28 18:39:46 -0700

    Conjunctions of sentences

--- rules_CS.pl
+++ rules_CS.pl
@@ -1,9 +1,13 @@
 utterance(X) :- sentence(X, []).
+utterance(X) :- sentence(X, [Conjunction | Rest]), conjunction(Conjunction), utterance(Rest).
 sentence(Start, End) :- nounphrase(Start, Rest, Number), verbphrase(Rest, End, Number).
 
 tail(List, Tail) :- append(_, [Tail], List). %from SWI-Prolog documentation
-allbuttail([_], Allbuttail) :- Allbuttail = [].
-allbuttail(List, Allbuttail) :- length(List, Length), Length > 1, tail(List, Tail), append(List, Tail, All), prefix(Allbuttail, All).
+head([Head | _], Head).
+nonempty(List) :- length(List, Length), Length > 0.
+allbuthead([_ | Rest], Rest).
+allbuttail([_], Allbuttail) :- Allbuttail == [].
+allbuttail(List, Allbuttail) :- length(List, Length), Length > 1, tail(List, Tail), append(List, Tail, All), prefix(Allbuttail, All), length(Allbuttail, Returnlength), Returnlength =:= Length - 1.
 all([], _).
 all([Head | Rest], Predicate) :- call(Predicate, Head), all(Rest, Predicate).
 
@@ -13,6 +17,7 @@ adjective(Adjective) :- adjective(_, Adjective).
 article(Article, Number) :- article(_, Article, Number).
 adverb(Adverb) :- adverb(_, Adverb).
 copula(Copula, Number) :- copula(_, Copula, Number).
+conjunction(Conjunction) :- conjunction(_, Conjunction).
 
 nounphrase(Adjectivephrase, End, Number) :- adjectivephrase(Adjectivephrase, End, Number).
 nounphrase([Article | Rest], End, Number) :- article(Article, Number), adjectivephrase(Rest, End, Number).
@@ -27,4 +32,6 @@ verbphrase([Copula | Object], End, Number) :- copula(Copula, Number), copularobj
 verbphrase(Verbphrase, End, Number) :- tail(Verbphrase, Tail), allbuttail(Verbphrase, Prefix), verbphrase(Prefix, End, Number), adverb(Tail).
 
 copularobject([Article | Rest], Number, End) :- article(Article, Number), adjectivephrase(Rest, End, Number).
+copularobject([Adjective | Rest], _, Rest) :- adjective(Adjective).
 copularobject(Object, _, _) :- allbuttail(Object, Head), all(Head, adverb), tail(Object, Tail), adjective(Tail).
+copularobject([], _, _).
